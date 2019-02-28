17adab29d5e0e18d5c1e68f1be686e11965e07ce
abderahmane44 <46277909+abderahmane44@users.noreply.github.com>
2019-02-28 11:54:01 -1000

    Update bot.js

--- bot.js
+++ bot.js
@@ -11,62 +11,6 @@ console.log("log");
 
 
 
-
-client.on("message", msg => {//Alpha Codes 
-    var Alpha = '+';//البرفكس
-    if(msg.content.startsWith(Alpha + "invite")){//Alpha Codes 
-        let e = new Discord.RichEmbed()//Alpha Codes 
-        .setTitle("**اضافه البوت لسيرفرك**")//Alpha Codes 
-       .setDescription(`**📬 | اذا تريد البوت يرسلك الرابط بخاصك
-       📇 | اذا تريد البوت يرسلك الرابط هنا بالشات**`)
-        msg.channel.send(e).then(b => {
-            b.react('📇')
-            .then(() => b.react('📬'))
-            .then(() =>b.react('📇'))
-            let reaction1Filter = (reaction, user) => reaction.emoji.name === '📇' && user.id === msg.author.id;
-let reaction2Filter = (reaction, user) => reaction.emoji.name === '📬' && user.id === msg.author.id;
-
-let reaction1 = b.createReactionCollector(reaction1Filter, { time: 12000 });
-let reaction2 = b.createReactionCollector(reaction2Filter, { time: 12000 });
-reaction1.on("collect", r => {
-msg.reply(`https://discordapp.com/api/oauth2/authorize?client_id=${client.user.id}&permissions=0&scope=bot`)
-b.delete(2000)
-})
-reaction2.on("collect", r => {
-    msg.author.send(`${msg.author} https://discordapp.com/api/oauth2/authorize?client_id=${client.user.id}&permissions=0&scope=bot`)
-    b.delete(2000)
-    msg.reply("**تم ارسال الرابط في خاصك 📬**").then(d => {
-        d.delete(2000)
-    })
-    })
-        })
-    }
-});
-
-
-client.on('message', function(msg) {
-var prefix = '+';//البرفكس
-    if(msg.content.startsWith (prefix + 'server')) {
-      if(!msg.channel.guild) return msg.reply('**❌ اسف لكن هذا الامر للسيرفرات فقط **');
-      let embed = new Discord.RichEmbed()
-      .setColor('RANDOM')
-      .setThumbnail(msg.guild.iconURL)
-      .addField('🌐 **اسم السيرفر : **' , `**[ ${msg.guild.name} ]**`,true)
-      .addField('🌍 ** موقع السيرفر :**',`**[ ${msg.guild.region} ]**`,true)
-      .addField('🎖** الرتب :**',`**[ ${msg.guild.roles.size} ]**`,true)
-      .addField('👤** عدد الاعضاء :**',`**[ ${msg.guild.memberCount} ]**`,true)
-      .addField('✅** عدد الاعضاء الاونلاين :**',`**[ ${msg.guild.members.filter(m=>m.presence.status == 'online').size} ]**`,true)
-      .addField('📝** الرومات الكتابية :**',`**[ ${msg.guild.channels.filter(m => m.type === 'text').size} ]**`,true)
-      .addField('🔊** رومات الصوت :**',`**[ ${msg.guild.channels.filter(m => m.type === 'voice').size} ]**`,true)
-      .addField('👑** حبي الاونر :**',`**[ ${msg.guild.owner} ]**`,true)
-      .addField('🆔** ايدي السيرفر :**',`**[ ${msg.guild.id} ]**`,true)
-      .addField('📅** تم عمل السيرفر في : **',msg.guild.createdAt.toLocaleString())
-      .addField('😴** روم AFK**',`**${msg.guild.afkChannel || 'None'}**`, true)
-      .addField('🙃** الايموجيات :**', `**${msg.guild.emojis.size}** \`[\` **${msg.guild.emojis.map(m => m).join('**|**')} \`]\`**`, true);
-      msg.channel.send({embed:embed});
-    }
-  });///////////////ALPHA CODES //// MAHMOUD QUSTYLE
-
  
 client.on('message', message => {
     if (message.content === '....') {
@@ -97,7 +41,6 @@ client.on('message', message => {
 
 
    
-
 
 
 
