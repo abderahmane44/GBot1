2614f5956c7c36762b76efce15ecd615338055df
abderahmane44 <46277909+abderahmane44@users.noreply.github.com>
2019-02-28 11:55:57 -1000

    Update bot.js

--- bot.js
+++ bot.js
@@ -42,10 +42,65 @@ client.on('message', message => {
 
    
 
+const gamestats = [`Mazza Shop`,`Mazza Shop`,`Mazza Shop`,`Mazza Shop`]
+var index = 0
+var timer = 10 // الوقت بالثواني لتغير الستريمنق
+client.on("ready", ()=> {
+        setInterval(function(){
+        client.user.setGame(`${gamestats[index]}`,'https://www.twitch.tv/ACMBOT') 
+        index++
+            if( index >= gamestats.length) index = 0 ;
+        }, timer*1000);
+
+});
 
 
 
 
-	
-	
-client.login(process.env.BOT_TOKEN);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+client.login(process.env.TOKEN);
