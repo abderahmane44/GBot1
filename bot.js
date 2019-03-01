0c227e7ab1ccc31c08f4bd7583f2225943b79054
abderahmane44 <46277909+abderahmane44@users.noreply.github.com>
2019-03-01 08:11:39 -1000

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,15 +1,33 @@
-"const Discord = require("discord.js");
+const Discord = require('discord.js');
+const Canvas = require('canvas')
 const client = new Discord.Client();
-const bot = new Discord.Client();
-const ms = require("ms");
-const fs = require('fs');
-const moment = require('moment');
-const request = require('request');
-const dateFormat = require('dateformat');
-const r1 = require('snekfetch');
-const jimp = require('jimp')
-const math = require('math-expression-evaluator'); 
-
+const prefix = '+';
+ 
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+client.user.setGame(` =help | Legend System ✨ `,"https://www.twitch.tv/S-F")
+  console.log('')
+  console.log('')
+  console.log('╔[═════════════════════════════════════════════════════════════════]╗')
+  console.log(`[Start] ${new Date()}`);
+  console.log('╚[═════════════════════════════════════════════════════════════════]╝')
+  console.log('')
+  console.log('╔[════════════════════════════════════]╗');
+  console.log(`Logged in as * [ " ${client.user.username} " ]`);
+  console.log('')
+  console.log('Informations :')
+  console.log('')
+  console.log(`servers! [ " ${client.guilds.size} " ]`);
+  console.log(`Users! [ " ${client.users.size} " ]`);
+  console.log(`channels! [ " ${client.channels.size} " ]`);
+  console.log('╚[════════════════════════════════════]╝')
+  console.log('')
+  console.log('╔[════════════]╗')
+  console.log(' Bot Is Online')
+  console.log('╚[════════════]╝')
+  console.log('')
+  console.log('')
+});
 const prefix = "+";
 
 
@@ -89,7 +107,6 @@ client.on('ready', function(){//npm i ms
 //=================================== Commands ===================================
 
 
-const prefix = "+";
 
 client.on('message', message => {
             if(!message.channel.guild) return;
