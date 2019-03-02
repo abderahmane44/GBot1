ee0d3bfe5f1447ad792f200c3ed5b42790384c86
abderahmane44 <46277909+abderahmane44@users.noreply.github.com>
2019-03-02 05:06:01 -1000

    Update bot.js

--- bot.js
+++ bot.js
@@ -79,7 +79,18 @@ client.on('ready', () => {
   console.log(' Logged in as community Bot ')
 });
 
-
+client.on('message', message => {
+   if (message.content === "+id") {
+   let embed = new Discord.RichEmbed()
+  .setColor("RANDOM")
+  .setThumbnail(message.author.avatarURL)
+  .addField("Name:",`${message.author.username}`, true)
+  .addField('Discrim:',"#" + `${message.author.discriminator}`, true)
+  .addField("ID:", `${message.author.id}`, true)
+  .addField("Create At:", `${message.author.createdAt}`, true)
+  message.channel.sendEmbed(embed);
+    }
+});
 
 
 
