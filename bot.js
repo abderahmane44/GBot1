e038ff3f6fe59b7e7f7ada4239aa9b08ea2bfc19
abderahmane44 <46277909+abderahmane44@users.noreply.github.com>
2019-03-02 05:16:12 -1000

    Update bot.js

--- bot.js
+++ bot.js
@@ -132,7 +132,38 @@ client.on('message', message => {
 
 
 
+client.on('message', message => {
+    if (message.content === "+rooms") {
+        if (message.author.bot) return
+                      if (!message.guild) return;
 
+        var channels = message.guild.channels.map(channels => `${channels.name}, `).join(' ')
+        const embed = new Discord.RichEmbed()
+        .setColor('RANDOM')
+        .addField(`${message.guild.name}`,`**Rooms:white_check_mark:**`)
+        .addField('⬇ Rooms Number. ✔',`** ${message.guild.channels.size}**`)
+        
+.addField('⬇Rooms  Name. ✔:',`**[${channels}]**`)
+        message.channel.sendEmbed(embed);
+    }
+});
+
+var AsciiTable = require('ascii-data-table').default
+client.on('message', message =>{
+
+    if(message.content == "+roles"){
+        var 
+        ros=message.guild.roles.size,
+        data = [['Rank', 'RoleName']]
+        for(let i =0;i<ros;i++){
+            if(message.guild.roles.array()[i].id !== message.guild.id){
+         data.push([i,`${message.guild.roles.filter(r => r.position == ros-i).map(r=>r.name)}`])
+        }}
+        let res = AsciiTable.table(data)
+
+        message.channel.send(`**\`\`\`xl\n${res}\`\`\`**`);
+    }
+});//Toxic Codes
 
 
 
