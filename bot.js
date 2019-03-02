55c1dcbcc87b2faa937e90be0f01e2a1a85f1389
abderahmane44 <46277909+abderahmane44@users.noreply.github.com>
2019-03-02 04:23:10 -1000

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,10 +1,23 @@
-const Discord = require('discord.js');
-const fs = require('fs');
+const Discord = require("discord.js");
 const client = new Discord.Client();
 
 client.on('ready', () => {
-  console.log(`Welcome Bro ${client.user.tag}!`);
+  console.log(`Logged in as ${client.user.tag}!`);
 });
+
+client.on('message', msg => {
+  if (msg.content === 'ping') {
+    msg.reply('Pong!');
+  }
+});
+
+client.on('ready',  () => {
+  console.log('By : حسوني العراقي ');
+  console.log(`Logged in as * [ " ${client.user.username} " ] servers! [ " ${client.guilds.size} " ]`);
+  console.log(`Logged in as * [ " ${client.user.username} " ] Users! [ " ${client.users.size} " ]`);
+  console.log(`Logged in as * [ " ${client.user.username} " ] channels! [ " ${client.channels.size} " ]`);
+});
+
 const prefix = '_'
 
 client.on('message', message => {
