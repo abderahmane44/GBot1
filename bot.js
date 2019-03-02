3fe0bb23458a37fa2a8a70edf2b57b500bf0c846
abderahmane44 <46277909+abderahmane44@users.noreply.github.com>
2019-03-02 04:09:25 -1000

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,33 +1,3 @@
-const Discord = require('discord.js');
-const ytdl = require('ytdl-core');
-const { Client, Util } = require('discord.js');  
-const getYoutubeID = require('get-youtube-id'); 
-const fetchVideoInfo = require('youtube-info');
-const YouTube = require('simple-youtube-api');
-const yt_api_key = "AIzaSyDeoIH0u1e72AtfpwSKKOSy3IPp2UHzqi4";
-const request = require('request');
-const queue = new Map(); 
-const client = new Discord.Client(); 
-const db = require('quick.db');
-const giphy = require('giphy-api')();    
-const googl = require('goo.gl'); 
-const translate = require('google-translate-api');   
-const fs = require("fs"); 
-var data = JSON.parse(fs.readFileSync('data.json','utf8'))
-const moment = require("moment");
-const UserBlocked = new Set(); 
-const jimp = require('jimp');   
-const math = require('math-expression-evaluator'); 
-const stripIndents = require('common-tags').stripIndents;
-const figlet = require('figlet');
-const google = require('google-it'); 
-const zalgo = require('zalgolize');   
-const sql = require("sqlite");
- const dateFormat = require('dateformat'); 
- const pretty = require('pretty-ms') 
-,ti={}  
-,spee={};
- const prefix = "*";
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
